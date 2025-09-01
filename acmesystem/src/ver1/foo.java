f2e1fadd5571a6a1799818df9c9ad10c286de46f
Logan Rinberger <logan.rinberger@gmail.com>
2025-08-31 23:00:52 -0400

    commit

--- acmesystem/src/ver1/foo.java
+++ acmesystem/src/ver1/foo.java
@@ -1,9 +1,10 @@
 package ver1;
 
-public class foo {
+public class foo 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 
 	}
 
